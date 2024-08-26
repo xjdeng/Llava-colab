a8e09adde04aa8e6c3426666b18a9616cb87adf2
Xiaojian "JJ" Deng <xjd001@gmail.com>
2024-08-26 11:36:41 -0400

    Update backend.py

--- backend.py
+++ backend.py
@@ -2,10 +2,16 @@ import os
 import requests
 from PIL import Image
 from io import BytesIO
-from llava.conversation import conv_templates, SeparatorStyle
-from llava.utils import disable_torch_init
-from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
-from llava.mm_utils import tokenizer_image_token, get_model_name_from_path, KeywordsStoppingCriteria
+try:
+  from .llava.conversation import conv_templates, SeparatorStyle
+  from .llava.utils import disable_torch_init
+  from .llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
+  from .llava.mm_utils import tokenizer_image_token, get_model_name_from_path, KeywordsStoppingCriteria
+except ImportError:
+  from llava.conversation import conv_templates, SeparatorStyle
+  from llava.utils import disable_torch_init
+  from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
+  from llava.mm_utils import tokenizer_image_token, get_model_name_from_path, KeywordsStoppingCriteria
 from transformers import TextStreamer
 from transformers import AutoTokenizer, BitsAndBytesConfig
 from llava.model import LlavaLlamaForCausalLM
